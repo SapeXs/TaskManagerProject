daad78019286a95e483e9e72b6ecd9b0c80a5df4
Сергей Пфлюг <pflug.s@phystech.edu>
2026-03-23 11:53:48 +0300

    добавлены классы прогресса заданий

---
+++ include/progress/fraction_progress.h
@@ -0,0 +1,49 @@
+#pragma once
+
+#include <cstdint>
+#include <string>
+
+#include "../core/task_state.h"
+
+class FractionProgress {
+ public:
+  FractionProgress(int32_t done, int32_t total)
+      : done_(done), total_(total) {
+  }
+
+  int32_t GetDone() const noexcept {
+    return done_;
+  }
+
+  int32_t GetTotal() const noexcept {
+    return total_;
+  }
+
+  void IncrementDone(int32_t value = 1) noexcept {
+    done_ += value;
+    if (done_ < 0) {
+      done_ = 0;
+    }
+    if (done_ > total_) {
+      done_ = total_;
+    }
+  }
+
+  std::string ToString() const {
+    return std::to_string(done_) + "/" + std::to_string(total_);
+  }
+
+  TaskState GetState() const noexcept {
+    if (total_ == 0 || done_ == 0) {
+      return TaskState::kNotStarted;
+    }
+    if (done_ >= total_) {
+      return TaskState::kDone;
+    }
+    return TaskState::kInProgress;
+  }
+
+ private:
+  int32_t done_;
+  int32_t total_;
+};
---
+++ include/progress/percentage_progress.h
@@ -0,0 +1,57 @@
+#pragma once
+
+#include <cstdint>
+#include <string>
+
+#include "../core/task_state.h"
+
+class PercentageProgress {
+ public:
+  PercentageProgress(int64_t current_value, int64_t target_value)
+      : current_value_(current_value), target_value_(target_value) {
+  }
+
+  int64_t GetCurrentValue() const noexcept {
+    return current_value_;
+  }
+
+  int64_t GetTargetValue() const noexcept {
+    return target_value_;
+  }
+
+  void Add(int64_t value) noexcept {
+    current_value_ += value;
+    if (current_value_ < 0) {
+      current_value_ = 0;
+    }
+  }
+
+  int32_t GetPercent() const noexcept {
+    if (target_value_ <= 0 || current_value_ <= 0) {
+      return 0;
+    }
+    if (current_value_ >= target_value_) {
+      return 100;
+    }
+    return static_cast<int32_t>(current_value_ * 100 / target_value_);
+  }
+
+  std::string ToString() const {
+    return std::to_string(GetPercent()) + "%";
+  }
+
+  TaskState GetState() const noexcept {
+    int32_t percent = GetPercent();
+    if (percent == 0) {
+      return TaskState::kNotStarted;
+    }
+    if (percent >= 100) {
+      return TaskState::kDone;
+    }
+    return TaskState::kInProgress;
+  }
+
+ private:
+  int64_t current_value_;
+  int64_t target_value_;
+};
