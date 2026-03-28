e3e6da73703f85c4f7471984f6e1f714ddd1633a
Сергей Пфлюг <pflug.s@phystech.edu>
2026-03-28 20:30:31 +0300

    добавлена класс-задача копилка

---
+++ include/tasks/savings_task.h
@@ -0,0 +1,25 @@
+#pragma once
+
+#include <cstdint>
+#include <string>
+#include <utility>
+
+#include "../core/task_priority.h"
+#include "../core/task_with_progress.h"
+#include "../progress/percentage_progress.h"
+
+class SavingsTask : public TaskWithProgress<PercentageProgress> {
+ public:
+  using TagContainer = TaskBase::TagContainer;
+
+  SavingsTask(int32_t id, std::string title, std::string description,
+              TaskPriority priority, TagContainer tags, int64_t current_value,
+              int64_t target_value);
+
+  std::string GetTypeName() const override;
+  
+  int64_t GetCurrentValue() const noexcept;
+  int64_t GetTargetValue() const noexcept;
+
+  void AddValue(int64_t value) noexcept;
+};
