4a02b0410064d0ad981785748f03a3756d92dec6
Сергей Пфлюг <pflug.s@phystech.edu>
2026-03-28 20:40:34 +0300

    добавлена класс-задача напоминалка

---
+++ include/tasks/reminder_task.h
@@ -0,0 +1,21 @@
+#pragma once
+
+#include <cstdint>
+#include <string>
+
+#include "../core/task_priority.h"
+#include "../core/task_with_progress.h"
+#include "../progress/time_left_progress.h"
+
+class ReminderTask : public TaskWithProgress<TimeLeftProgress> {
+ public:
+  using TagContainer = TaskBase::TagContainer;
+
+  ReminderTask(int32_t id, std::string title, std::string description,
+               TaskPriority priority, TagContainer tags,
+               int64_t seconds_left);
+
+  std::string GetTypeName() const override;
+
+  void SetSecondsLeft(int64_t seconds_left) noexcept;
+};
