9b742361598167ae9401eb7f649613833aa90a80
Сергей Пфлюг <pflug.s@phystech.edu>
2026-04-16 19:35:20 +0300

    написан функционал базового класса

--- include/core/task_base.h
+++ include/core/task_base.h
@@ -15,31 +15,26 @@ public:
   TaskBase(int32_t id, std::string title, std::string description,
            TaskPriority priority, TagContainer tags);
 
-  /*virtual ~TaskBase() = default;
-  TaskBase(const TaskBase&) = default;
-  TaskBase(TaskBase&&) noexcept = default;
-  TaskBase& operator=(const TaskBase&) = default;
-  TaskBase& operator=(TaskBase&&) noexcept = default;*/
-
   virtual std::string GetTypeName() const = 0;
   virtual TaskState GetState() const = 0;
   virtual std::string GetProgressString() const = 0;
 
   int32_t GetId() const noexcept;
-  const std::string &GetTitle() const noexcept;
-  const std::string &GetDescription() const noexcept;
+  const std::string& GetTitle() const noexcept;
+  const std::string& GetDescription() const noexcept;
   TaskPriority GetPriority() const noexcept;
-  const TagContainer &GetTags() const noexcept;
+  const TagContainer& GetTags() const noexcept;
 
-  bool HasTag(std::string_view tag) const;
-  void AddTag(std::string tag);
-  void RemoveTag(std::string_view tag);
+  bool HasTag(std::string& tag) const;
+  void AddTag(std::string& tag);
+  void RemoveTag(std::string& tag);
 
 protected:
-  void SetTitle(std::string title);
-  void SetDescription(std::string description);
-  void SetPriority(TaskPriority priority);
-  void SetTags(TagContainer tags);
+  void SetTitle(std::string& title);
+  void SetDescription(std::string& description);
+  void SetPriority(TaskPriority& priority);
+  void SetTags(TagContainer& tags);
+  void SetTags(std::string& tag);
 
 private:
   int32_t id_;
