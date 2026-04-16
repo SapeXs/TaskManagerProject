256445259cc8066fa6883694c87ae7571e3a2b0b
Сергей Пфлюг <pflug.s@phystech.edu>
2026-04-16 18:52:37 +0300

    весь функционал класса задача с прогрессом перенесён в cpp

--- include/core/task_with_progress.h
+++ include/core/task_with_progress.h
@@ -3,8 +3,6 @@
 #include <concepts>
 #include <cstdint>
 #include <string>
-#include <unordered_set>
-#include <utility>
 
 #include "task_base.h"
 #include "task_priority.h"
@@ -22,25 +20,13 @@ public:
 
   TaskWithProgress(int32_t id, std::string title, std::string description,
                    TaskPriority priority, TagContainer tags,
-                   int64_t seconds_left)
-      : TaskBase(id, std::move(title), std::move(description), priority,
-                 std::move(tags)),
-        progress_(seconds_left) {}
+                   int64_t seconds_left);
 
-  /*~TaskWithProgress() override = default;
-  TaskWithProgress(const TaskWithProgress&) = default;
-  TaskWithProgress(TaskWithProgress&&) noexcept = default;
-  TaskWithProgress& operator=(const TaskWithProgress&) = default;
-  TaskWithProgress& operator=(TaskWithProgress&&) noexcept = default;*/
-
-  TaskState GetState() const override { return progress_.GetState(); }
-
-  std::string GetProgressString() const override {
-    return progress_.ToString();
-  }
+  TaskState GetState() const;
+  std::string GetProgressString() const;
 
 protected:
-  ProgressType& GetProgress() { return progress_; }
+  ProgressType& GetProgress();
 
 private:
   ProgressType progress_;
---
+++ src/core/task_with_progress.cpp
@@ -0,0 +1,28 @@
+#include "core/task_with_progress.h"
+
+using TagContainer = TaskBase::TagContainer;
+
+template <ProgressLike ProgressType>
+TaskWithProgress<ProgressType>::TaskWithProgress(int32_t id, std::string title,
+                                                 std::string description,
+                                                 TaskPriority priority,
+                                                 TagContainer tags,
+                                                 int64_t seconds_left)
+    : TaskBase(id, std::move(title), std::move(description), priority,
+               std::move(tags)),
+      progress_(seconds_left){};
+
+template <ProgressLike ProgressType>
+TaskState TaskWithProgress<ProgressType>::GetState() const {
+    return progress_.GetState();
+}
+
+template <ProgressLike ProgressType>
+std::string TaskWithProgress<ProgressType>::GetProgressString() const {
+    return progress_.ToString();
+}
+
+template <ProgressLike ProgressType>
+ProgressType& TaskWithProgress<ProgressType>::GetProgress() {
+    return progress_;
+}
