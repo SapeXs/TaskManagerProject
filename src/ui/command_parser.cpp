9e0ec7fca2cdae25d21c93b241f872b8542cd18d
egor.kazakevich <kazakevich.e@pystech.edu>
2026-05-04 00:16:10 +0300

    добавил Command_Parser с хеш-таблицей и переделал move

--- src/ui/command_parser.cpp
+++ src/ui/command_parser.cpp
@@ -1,12 +1,13 @@
 #include "ui/command_parser.h"
 #include <sstream>
 #include <string>
+#include <unordered_map>
 
 std::vector<std::string> CommandParser::Split_(const std::string& input) const {
     std::vector<std::string> tokens;
     std::istringstream stream(input);
     std::string token;
-
+    
     while (stream >> token) {
         tokens.push_back(std::move(token));
     }
@@ -14,27 +15,29 @@ std::vector<std::string> CommandParser::Split_(const std::string& input) const {
 }
 
 CommandType CommandParser::ParseCommandType_(const std::string& command_name) const {
-    if (command_name == "add")      return CommandType::kAdd;
-    if (command_name == "list")     return CommandType::kList;
-    if (command_name == "find")     return CommandType::kFind;
-    if (command_name == "remove")   return CommandType::kRemove;
-    if (command_name == "filter")   return CommandType::kFilter;
-    if (command_name == "help")     return CommandType::kHelp;
-    if (command_name == "exit")     return CommandType::kExit;
+    static const std::unordered_map<std::string, CommandType> kCommandMap = {
+        {"add",    CommandType::kAdd},
+        {"list",   CommandType::kList},
+        {"find",   CommandType::kFind},
+        {"remove", CommandType::kRemove},
+        {"filter", CommandType::kFilter},
+        {"help",   CommandType::kHelp},
+        {"exit",   CommandType::kExit}
+    };
 
-    return CommandType::kInvalid;
+    auto it = kCommandMap.find(command_name);
+    return (it != kCommandMap.end()) ? it->second : CommandType::kInvalid;
 }
 
 Command CommandParser::Parse(const std::string& input) const {
     std::vector<std::string> tokens = Split_(input);
-
+    
     if (tokens.empty()) {
         return Command(CommandType::kInvalid, {});
     }
 
-    CommandType type = ParseCommandType_(tokens[0]);
-    
-    std::vector<std::string> args(tokens.begin() + 1, tokens.end());
-
-    return Command(type, std::move(args));
+    return Command(
+        ParseCommandType_(tokens[0]),
+        std::vector<std::string>(tokens.begin() + 1, tokens.end())
+    );
 }
