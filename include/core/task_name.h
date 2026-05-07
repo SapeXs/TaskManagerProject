2d5cdc34b2c2a81aa6ab966ee7f0f71d181ab13a
Сергей Пфлюг <pflug.s@phystech.edu>
2026-05-07 21:47:04 +0300

    создан файл с константами для имён задач

---
+++ include/core/task_name.h
@@ -0,0 +1,12 @@
+#pragma once
+
+#include <string>;
+
+namespace {
+  const std::string kNameBoundedRecurringTask = "Bounded Recurring Task";
+  const std::string kNameFinalDeadlineTask = "Final Deadline Task";
+  const std::string kNameRecurringTask = "Recurring Task";
+  const std::string kNameReminderTask = "Reminder Task";
+  const std::string kNameSavingTask = "Saving Task";
+  const std::string kNameSteppedDeadlineTask = "Stepped Deadline Task";
+}
