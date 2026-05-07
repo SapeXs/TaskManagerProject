947f138bc482a29287a43cf0cb9a58caa5a13227
Сергей Пфлюг <pflug.s@phystech.edu>
2026-05-07 22:41:16 +0300

    изменён конструктор класса

--- src/tasks/reminder_task.cpp
+++ src/tasks/reminder_task.cpp
@@ -6,7 +6,7 @@ ReminderTask::ReminderTask(int32_t id, std::string title,
                            TagContainer tags, int64_t seconds_left)
     : TaskWithProgress<TimeLeftProgress>(id, std::move(title),
                                          std::move(description), priority,
-                                         std::move(tags), seconds_left) {}
+                                         std::move(tags)) {GetProgress().SetSecondsLeft(seconds_left);}
 
 std::string ReminderTask::GetTypeName() const {
     return kNameReminderTask;
