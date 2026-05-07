e01b9cd56d9e95200e9d18c010ef35b594aefe2d
Сергей Пфлюг <pflug.s@phystech.edu>
2026-05-08 00:24:37 +0300

    изменён кодстайл

--- src/core/task_base.cpp
+++ src/core/task_base.cpp
@@ -4,65 +4,43 @@ using TagContainer = std::unordered_set<std::string>;
 
 TaskBase::TaskBase(int32_t id, std::string title, std::string description,
                    TaskPriority priority, TagContainer tags)
-    : id_(id),
-      title_(std::move(title)),
-      description_(std::move(description)),
-      priority_(priority),
-      tags_(std::move(tags)) {}
+    : id_(id), title_(std::move(title)), description_(std::move(description)),
+      priority_(priority), tags_(std::move(tags)) {}
 
-int32_t TaskBase::GetId() const noexcept {
-    return id_;
+int32_t TaskBase::GetId() const noexcept { return id_; }
+
+const std::string &TaskBase::GetTitle() const noexcept { return title_; }
+
+const std::string &TaskBase::GetDescription() const noexcept {
+  return description_;
 }
 
-const std::string& TaskBase::GetTitle() const noexcept {
-    return title_;
+TaskPriority TaskBase::GetPriority() const noexcept { return priority_; }
+
+const TagContainer &TaskBase::GetTags() const noexcept { return tags_; }
+
+bool TaskBase::HasTag(const std::string &tag) const {
+  if (tags_.find(tag) != tags_.end()) {
+    return true;
+  }
+  return false;
 }
 
-const std::string& TaskBase::GetDescription() const noexcept {
-    return description_;
-}
+void TaskBase::AddTag(const std::string &tag) { tags_.insert(tag); }
 
-TaskPriority TaskBase::GetPriority() const noexcept {
-    return priority_;
-}
+void TaskBase::RemoveTag(const std::string &tag) { tags_.erase(tag); }
 
-const TagContainer& TaskBase::GetTags() const noexcept {
-    return tags_;
-}
-
-bool TaskBase::HasTag(const std::string& tag) const {
-    if (tags_.find(tag) != tags_.end()) {
-        return true;
-    } else {
-        return false;
-    }
-}
-
-void TaskBase::AddTag(const std::string& tag) {
-    tags_.insert(tag);
-}
-
-void TaskBase::RemoveTag(const std::string& tag) {
-    tags_.erase(tag);
-}
-
-void TaskBase::SetTitle(std::string title) {
-    title_ = std::move(title);
-}
+void TaskBase::SetTitle(std::string title) { title_ = std::move(title); }
 
 void TaskBase::SetDescription(std::string description) {
-    description_ = std::move(description);
+  description_ = std::move(description);
 }
 
-void TaskBase::SetPriority(TaskPriority priority) {
-    priority_ = priority;
-}
+void TaskBase::SetPriority(TaskPriority priority) { priority_ = priority; }
 
-void TaskBase::SetTags(TagContainer tags) {
-    tags_ = std::move(tags);
-}
+void TaskBase::SetTags(TagContainer tags) { tags_ = std::move(tags); }
 
 void TaskBase::SetTags(std::string tag) {
-    tags_.clear();
-    tags_.insert(std::move(tag));
+  tags_.clear();
+  tags_.insert(std::move(tag));
 }
