232fa9b12f90318c9e5675e47c21d8ff51d8edc9
Сергей Пфлюг <pflug.s@phystech.edu>
2026-05-07 22:13:24 +0300

    добавлены и использованы константы

--- include/progress/time_left_progress.h
+++ include/progress/time_left_progress.h
@@ -5,6 +5,19 @@
 
 #include "core/task_state.h"
 
+namespace {
+  constexpr int64_t kSecondsInMinute = 60;
+  constexpr int64_t kMinutesInHour = 60;
+  constexpr int64_t kHoursInDay = 24;
+  constexpr int64_t kZero = 0;
+
+  constexpr std::string kEventPassed = "event passed";
+  constexpr std::string kSecondsLeft = " sec left";
+  constexpr std::string kMinutesLeft = " min left";
+  constexpr std::string kHoursLeft = " hours left";
+  constexpr std::string kDaysLeft = " days left";
+}
+
 class TimeLeftProgress {
  public:
   explicit TimeLeftProgress(int64_t seconds_left)
@@ -20,30 +33,30 @@ class TimeLeftProgress {
   }
 
   std::string ToString() const {
-    if (seconds_left_ < 0) {
-      return "event passed";
+    if (seconds_left_ < kZero) {
+      return kEventPassed;
     }
 
-    if (seconds_left_ < 60) {
-      return std::to_string(seconds_left_) + " sec left";
+    if (seconds_left_ < kSecondsInMinute) {
+      return std::to_string(seconds_left_) + kSecondsLeft;
     }
 
-    int64_t minutes = seconds_left_ / 60;
-    if (minutes < 60) {
-      return std::to_string(minutes) + " min left";
+    int64_t minutes = seconds_left_ / kSecondsInMinute;
+    if (minutes < kMinutesInHour) {
+      return std::to_string(minutes) + kMinutesLeft;
     }
 
-    int64_t hours = minutes / 60;
-    if (hours < 24) {
-      return std::to_string(hours) + " hours left";
+    int64_t hours = minutes / kMinutesInHour;
+    if (hours < kHoursInDay) {
+      return std::to_string(hours) + kHoursLeft;
     }
 
-    int64_t days = hours / 24;
-    return std::to_string(days) + " days left";
+    int64_t days = hours / kHoursInDay;
+    return std::to_string(days) + kDaysLeft;
   }
 
   TaskState GetState() const noexcept {
-    if (seconds_left_ < 0) {
+    if (seconds_left_ < kZero) {
       return TaskState::kOverdue;
     }
     return TaskState::kScheduled;
