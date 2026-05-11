16221eea0aee678b83992360fb89c059d4e77793
egor.kazakevich <kazakevich.e@pystech.edu>
2026-05-11 22:19:43 +0300

    добавил перечисление CommandType

--- include/ui/command_type.h
+++ include/ui/command_type.h
@@ -1,18 +1,12 @@
 #pragma once
 
-#include <vector>
-#include <string>
-#include "ui/command_type.h"
-
-class Command {
-public:
-    Command(CommandType type, std::vector<std::string> args = {});
-
-    CommandType GetType() const noexcept;
-    const std::vector<std::string>& GetArgs() const noexcept;
-    bool IsValid() const noexcept;
-
-private:
-    CommandType type_;
-    std::vector<std::string> args_;
+enum class CommandType {
+  kAdd,
+  kList,
+  kFind,
+  kRemove,
+  kFilter,
+  kHelp,
+  kExit,
+  kInvalid,
 };
