cc65087285544252e430aa7c55fec22f7303a0b3
egor.kazakevich <kazakevich.e@pystech.edu>
2026-05-11 23:40:21 +0300

    реализовал логику парсинга команд с хеш-таблицей

--- src/ui/command_parser.cpp
+++ src/ui/command_parser.cpp
@@ -1,9 +1,24 @@
 #include "ui/command_parser.h"
 #include <sstream>
+#include <string>
 #include <unordered_map>
+
+namespace {
+const std::unordered_map<std::string, CommandType> kCommandMap = {
+    {"add",    CommandType::kAdd},
+    {"list",   CommandType::kList},
+    {"find",   CommandType::kFind},
+    {"remove", CommandType::kRemove},
+    {"filter", CommandType::kFilter},
+    {"help",   CommandType::kHelp},
+    {"exit",   CommandType::kExit}
+};
+}  // namespace
 
 std::vector<std::string> CommandParser::Split_(const std::string& input) const {
   std::vector<std::string> tokens;
+  tokens.reserve(10);
+  
   std::istringstream stream(input);
   std::string token;
   while (stream >> token) {
@@ -13,21 +28,19 @@ std::vector<std::string> CommandParser::Split_(const std::string& input) const {
 }
 
 CommandType CommandParser::ParseCommandType_(const std::string& command_name) const {
-  static const std::unordered_map<std::string, CommandType> kCommandMap = {
-      {"add", CommandType::kAdd},     {"list", CommandType::kList},
-      {"find", CommandType::kFind},   {"remove", CommandType::kRemove},
-      {"filter", CommandType::kFilter}, {"help", CommandType::kHelp},
-      {"exit", CommandType::kExit}
-  };
   auto it = kCommandMap.find(command_name);
   return (it != kCommandMap.end()) ? it->second : CommandType::kInvalid;
 }
 
 Command CommandParser::Parse(const std::string& input) const {
   std::vector<std::string> tokens = Split_(input);
+  
   if (tokens.empty()) {
     return Command(CommandType::kInvalid, {});
   }
-  return Command(ParseCommandType_(tokens[0]),
-                 std::vector<std::string>(tokens.begin() + 1, tokens.end()));
+  
+  return Command(
+      ParseCommandType_(tokens[0]),
+      std::vector<std::string>(tokens.begin() + 1, tokens.end())
+  );
 }
