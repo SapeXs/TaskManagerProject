349680ae645de57461cf0c8785e016dd825c8234
egor.kazakevich <kazakevich.e@pystech.edu>
2026-05-11 23:50:17 +0300

    исправил пути include и удалил закомментированный код

--- include/core/task_base.h
+++ include/core/task_base.h
@@ -5,43 +5,37 @@
 #include <string_view>
 #include <unordered_set>
 
-#include "task_priority.h"
-#include "task_state.h"
+#include "core/task_priority.h"
+#include "core/task_state.h"
 
 class TaskBase {
-public:
+ public:
   using TagContainer = std::unordered_set<std::string>;
 
   TaskBase(int32_t id, std::string title, std::string description,
            TaskPriority priority, TagContainer tags);
-
-  /*virtual ~TaskBase() = default;
-  TaskBase(const TaskBase&) = default;
-  TaskBase(TaskBase&&) noexcept = default;
-  TaskBase& operator=(const TaskBase&) = default;
-  TaskBase& operator=(TaskBase&&) noexcept = default;*/
 
   virtual std::string GetTypeName() const = 0;
   virtual TaskState GetState() const = 0;
   virtual std::string GetProgressString() const = 0;
 
   int32_t GetId() const noexcept;
-  const std::string &GetTitle() const noexcept;
-  const std::string &GetDescription() const noexcept;
+  const std::string& GetTitle() const noexcept;
+  const std::string& GetDescription() const noexcept;
   TaskPriority GetPriority() const noexcept;
-  const TagContainer &GetTags() const noexcept;
-
+  const TagContainer& GetTags() const noexcept;
   bool HasTag(std::string_view tag) const;
+
   void AddTag(std::string tag);
   void RemoveTag(std::string_view tag);
 
-protected:
+ protected:
   void SetTitle(std::string title);
   void SetDescription(std::string description);
   void SetPriority(TaskPriority priority);
   void SetTags(TagContainer tags);
 
-private:
+ private:
   int32_t id_;
   std::string title_;
   std::string description_;
