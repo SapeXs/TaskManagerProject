f5e5a051a617fc8834a53fa8742f27e776c13321
egor.kazakevich <kazakevich.e@pystech.edu>
2026-05-11 23:50:17 +0300

    убрал лишние noexcept в FractionProgress

--- include/progress/fraction_progress.h
+++ include/progress/fraction_progress.h
@@ -7,26 +7,15 @@
 
 class FractionProgress {
  public:
-  FractionProgress(int32_t done, int32_t total)
-      : done_(done), total_(total) {
-  }
+  FractionProgress(int32_t done, int32_t total) : done_(done), total_(total) {}
 
-  int32_t GetDone() const noexcept {
-    return done_;
-  }
-
-  int32_t GetTotal() const noexcept {
-    return total_;
-  }
+  int32_t GetDone() const noexcept { return done_; }
+  int32_t GetTotal() const noexcept { return total_; }
 
   void IncrementDone(int32_t value = 1) noexcept {
     done_ += value;
-    if (done_ < 0) {
-      done_ = 0;
-    }
-    if (done_ > total_) {
-      done_ = total_;
-    }
+    if (done_ < 0) done_ = 0;
+    if (done_ > total_) done_ = total_;
   }
 
   std::string ToString() const {
@@ -34,12 +23,8 @@ class FractionProgress {
   }
 
   TaskState GetState() const noexcept {
-    if (total_ == 0 || done_ == 0) {
-      return TaskState::kNotStarted;
-    }
-    if (done_ >= total_) {
-      return TaskState::kDone;
-    }
+    if (total_ == 0 || done_ == 0) return TaskState::kNotStarted;
+    if (done_ >= total_) return TaskState::kDone;
     return TaskState::kInProgress;
   }
 
