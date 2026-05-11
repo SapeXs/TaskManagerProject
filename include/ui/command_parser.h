d913d9c384e87dced210696a004f254bc4fb33a3
egor.kazakevich <kazakevich.e@pystech.edu>
2026-05-11 22:19:43 +0300

    добавил заголовочный файл CommandParser

--- include/ui/command_parser.h
+++ include/ui/command_parser.h
@@ -1,17 +1,15 @@
 #pragma once
 
-#include <iostream>
 #include <string>
 #include <vector>
-#include "ui/command_type.h"
 #include "ui/command.h"
+#include "ui/command_type.h"
 
 class CommandParser {
-public:
-    Command Parse(const std::string& input) const;
+ public:
+  Command Parse(const std::string& input) const;
 
-private:
-    std::vector<std::string> Split_(const std::string& input) const;
-    CommandType ParseCommandType_(const std::string& command_name) const;
+ private:
+  std::vector<std::string> Split_(const std::string& input) const;
+  CommandType ParseCommandType_(const std::string& command_name) const;
 };
-
