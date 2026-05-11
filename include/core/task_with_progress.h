a2eb87ae422e7a7526f7535216d36ff08afdb38c
egor.kazakevich <kazakevich.e@pystech.edu>
2026-05-11 23:50:17 +0300

    исправил пути include в TaskWithProgress

--- include/core/task_with_progress.h
+++ include/core/task_with_progress.h
@@ -6,42 +6,33 @@
 #include <unordered_set>
 #include <utility>
 
-#include "task_base.h"
-#include "task_priority.h"
-#include "task_state.h"
+#include "core/task_base.h"
+#include "core/task_priority.h"
+#include "core/task_state.h"
 
 template <class ProgressType>
-concept ProgressLike = requires(const ProgressType &progress) {
+concept ProgressLike = requires(const ProgressType& progress) {
   { progress.ToString() } -> std::same_as<std::string>;
   { progress.GetState() } -> std::same_as<TaskState>;
 };
 
-template <ProgressLike ProgressType> class TaskWithProgress : public TaskBase {
-public:
+template <ProgressLike ProgressType>
+class TaskWithProgress : public TaskBase {
+ public:
   using TagContainer = TaskBase::TagContainer;
 
   TaskWithProgress(int32_t id, std::string title, std::string description,
-                   TaskPriority priority, TagContainer tags,
-                   int64_t seconds_left)
-      : TaskBase(id, std::move(title), std::move(description), priority,
-                 std::move(tags)),
+                   TaskPriority priority, TagContainer tags, int64_t seconds_left)
+      : TaskBase(id, std::move(title), std::move(description), priority, std::move(tags)),
         progress_(seconds_left) {}
 
-  /*~TaskWithProgress() override = default;
-  TaskWithProgress(const TaskWithProgress&) = default;
-  TaskWithProgress(TaskWithProgress&&) noexcept = default;
-  TaskWithProgress& operator=(const TaskWithProgress&) = default;
-  TaskWithProgress& operator=(TaskWithProgress&&) noexcept = default;*/
-
   TaskState GetState() const override { return progress_.GetState(); }
+  std::string GetProgressString() const override { return progress_.ToString(); }
 
-  std::string GetProgressString() const override {
-    return progress_.ToString();
-  }
-
-protected:
+ protected:
   ProgressType& GetProgress() { return progress_; }
+  const ProgressType& GetProgress() const { return progress_; }
 
-private:
+ private:
   ProgressType progress_;
 };
