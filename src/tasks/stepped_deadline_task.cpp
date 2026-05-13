f6630dd1f0ba4a5f89bc8e1be3ed7f498b528474
Сергей Пфлюг <pflug.s@phystech.edu>
2026-05-14 02:09:59 +0300

    переименован и переработан файл функционала класса

---
+++ src/tasks/stepped_deadline_task.cpp
@@ -0,0 +1,53 @@
+#include "tasks/stepped_deadline_task.h"
+#include "core/task_name.h"
+
+SteppedDeadlineTask::SteppedDeadlineTask(int32_t id, std::string title, std::string description,
+                                         TaskPriority priority, TagContainer tags,
+                                         std::vector<std::string> step_texts, size_t current_step)
+    : TaskWithProgress<TextProgress>(
+          id,
+          std::move(title),
+          std::move(description),
+          priority,
+          std::move(tags),
+          step_texts.empty() ? "" : step_texts[current_step],
+          TaskState::kInProgress), 
+          step_texts_(std::move(step_texts)), current_step_(current_step) {}
+
+std::string_view SteppedDeadlineTask::GetTypeName() const {
+    return kNameSteppedDeadlineTask;
+}
+
+const std::vector<std::string>& SteppedDeadlineTask::GetStepTexts() const noexcept {
+  return step_texts_;
+}
+
+size_t SteppedDeadlineTask::GetCurrentStep() const noexcept {
+  return current_step_;
+}
+
+void SteppedDeadlineTask::AdvanceStep() {
+  if (current_step_ + 1 < step_texts_.size()) {
+    ++current_step_;
+    UpdateProgressText(TaskState::kInProgress);
+    return;
+  }
+
+  UpdateProgressText(TaskState::kDone);
+}
+
+void SteppedDeadlineTask::SetOverdue() {
+  UpdateProgressText(TaskState::kOverdue);
+}
+
+void SteppedDeadlineTask::UpdateProgressText(
+    TaskState state) {
+  std::string text;
+
+  if (!step_texts_.empty() &&
+      current_step_ < step_texts_.size()) {
+    text = step_texts_[current_step_];
+  }
+
+  GetProgress().Set(std::move(text), state);
+}
