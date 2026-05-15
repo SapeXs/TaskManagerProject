12f335eec681b99e4a1a9d5bfae057f20c3e5405
Сергей Пфлюг <pflug.s@phystech.edu>
2026-05-15 20:40:29 +0300

    подправил стиль и добавил команду

--- src/ui/command_type.cpp
+++ src/ui/command_type.cpp
@@ -1,15 +1,25 @@
 #include "ui/command_type.h"
-#include <string>
+#include <string_view>
 
 std::string_view ToString(CommandType type) {
     switch (type) {
-        case CommandType::kAdd:      return "add";
-        case CommandType::kList:     return "list";
-        case CommandType::kFind:     return "find";
-        case CommandType::kRemove:   return "remove";
-        case CommandType::kFilter:   return "filter";
-        case CommandType::kHelp:     return "help";
-        case CommandType::kExit:     return "exit";
-        default:                     return "invalid";
+        case CommandType::kAdd:
+            return "add";
+        case CommandType::kList:
+            return "list";
+        case CommandType::kFind:
+            return "find";
+        case CommandType::kRemove:
+            return "remove";
+        case CommandType::kFilter:  
+            return "filter";
+        case CommandType::kHelp:
+            return "help";
+        case CommandType::kExit:
+            return "exit";
+        case CommandType::kSave:
+            return "save";
+        default:
+            return "invalid";
     }
 }
